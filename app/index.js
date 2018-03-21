57b3fe79467b7a0403d6825c069ef3e59a445080
ETBE <ebernoux@octo.com>
2018-03-21 11:14:04 +0100

    add app and index.js

---
+++ app/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/app1', function (req, res) {
+  res.send('1')
+})
+app.get('/app2', function (req, res) {
+  res.send('2')
+})
+app.get('/app3', function (req, res) {
+  res.send('3')
+})
+app.get('/app4', function (req, res) {
+  res.send('4')
+})
+app.get('/app5', function (req, res) {
+  res.send('5')
+})
+
+app.listen(PORT, function () {
+  console.log(`My monolyte listen on ${PORT}`)
+})

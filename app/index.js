d204e75c66ba75ea7ae4e32dde800afb9a01fe85
ETBE <ebernoux@octo.com>
2018-03-23 16:59:10 +0100

    add request id

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var addRequestId = require('express-request-id')();
 const morgan = require('morgan')
 const path = require('path')
 
@@ -6,7 +7,10 @@ const StreamLog = require("./StreamLog")
 
 const app = express()
 
+morgan.token('request_id', function (req, res) { return req.id })
+
 app.use(morgan('combined',{stream: new StreamLog()}))
+
 
 
 const PORT = process.env.PORT || 3000
@@ -14,6 +18,8 @@ const PORT = process.env.PORT || 3000
 app.get('/*', function (req, res) {
   res.sendStatus(200)
 })
+
+
 
 app.listen(PORT, function () {
   console.log(`My monolyte listen on ${PORT}`)

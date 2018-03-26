030f07bb4dc93c585042cfccef2de3fbc356bcf3
ETBE <ebernoux@octo.com>
2018-03-26 15:42:00 +0200

    add application log for XML

--- app/index.js
+++ app/index.js
@@ -5,23 +5,34 @@ const path = require('path')
 
 const StreamLog = require("./StreamLog")
 
+const PORT = process.env.PORT || 3000
+const APACHE_LOG_PORT = process.env.APACHE_LOG_PORT || 5101
+const APPLICATION_LOG_PORT = process.env.APPLICATION_LOG_PORT || 5200
+
 const app = express()
 
 const morganFormat = ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" :request_id'
-const apache_log_port = 5101
 
 morgan.token('request_id', function (req, res) { return req.id })
 
 app.use(addRequestId);
-app.use(morgan(morganFormat,{stream: new StreamLog(apache_log_port)}))
+app.use(morgan(morganFormat,{stream: new StreamLog(APACHE_LOG_PORT)}))
 
+const applicationLogStream = new StreamLog(APPLICATION_LOG_PORT)
 
-
-const PORT = process.env.PORT || 3000
+app.get('/XMLrequestConsumer', function (req, res) {
+	const log = {
+		request_id: req.id,
+		operation: "plafondSolde"
+	}
+	applicationLogStream.write
+	res.sendStatus(200)
+})
 
 app.get('/*', function (req, res) {
   res.sendStatus(200)
 })
+
 
 
 
--- app/logStashConnector.js
+++ app/logStashConnector.js
@@ -7,9 +7,13 @@ const  logHost = 'ec2-18-221-185-149.us-east-2.compute.amazonaws.com'
 function getLogStream(logPort) {
 	return new Promise((resolve,reject) => {
 	const conn = net.createConnection({host: logHost, port: logPort}, function() {
-		console.log('connected to server');
+		console.log(`connected to server ${logHost}:${logPort}`);
 		resolve(conn)
 		})
+	conn.on('error', () => {
+  		console.log(`error during connection to ${logHost}:${logPort}`);
+  		reject()
+		});
 	conn.on('end', () => {
   		console.log('disconnected from server');
   		reject()

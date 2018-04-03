f5bdb98be9d387b26c9164214b8a788d123ff8f7
ETBE <ebernoux@octo.com>
2018-04-03 16:37:32 +0200

    add logstash to monolythe

--- app/logStashConnector.js
+++ app/logStashConnector.js
@@ -1,7 +1,6 @@
 const net = require('net');
 
-
-const  logHost = 'ec2-18-221-185-149.us-east-2.compute.amazonaws.com'
+const  logHost = 'logstash'
   , sender = require('os').hostname();
 
 function getLogStream(logPort) {
@@ -11,21 +10,24 @@ function getLogStream(logPort) {
 }
 
 function _connect(logPort) {
-	console.log("try to connect")
 	return new Promise((resolve,reject) => {
-		const conn = net.createConnection({host: logHost, port: logPort}, function() {
-			console.log(`connected to server ${logHost}:${logPort}`)
-			resolve(conn)
-		})
-		conn.on('error', () => {
-			console.log(`error during connection to ${logHost}:${logPort}`);
-		})
-		conn.on('end', () => {
-	  		console.log('disconnected from server');
-	  	})
-		return conn
-	})
+    const conn = net.createConnection({host: logHost, port: logPort}, function() {
+      console.log(`connected to server ${logHost}:${logPort}`)
+      resolve(conn);
+    })
+    conn.on('error', () => {
+      console.log(`error during connection to ${logHost}:${logPort}`);
+      setTimeout(function() {
+        console.log(`retry`);
+        resolve (_connect(logPort));
+      }, 2000);
+    })
+    conn.on('end', () => {
+        console.log('disconnected from server');
+      })
+  })
 }
+
 
 
 
